1801f62177cecb21831f8255625f094bf7fcf078
Kirill <qtrixnet@yandex.ru>
2024-10-03 23:21:44 +0500

    feat/#11: protect routes

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ import { DarkModeSwitcher } from '@core/components'
 
 <template>
   <nav class="flex gap-4 bg-black text-white p-4 flex-wrap">
-    <RouterLink :to="ROUTES_PATHS.public.search">Поиск</RouterLink>
-    <RouterLink :to="ROUTES_PATHS.public.signIn">Войти</RouterLink>
-    <RouterLink :to="ROUTES_PATHS.private.duplicate">Дубликаты</RouterLink>
-    <RouterLink :to="ROUTES_PATHS.private.invalidReplacement">Невалидные замены</RouterLink>
-    <RouterLink :to="ROUTES_PATHS.private.obsoleteWithoutReplacement">Без замены</RouterLink>
-    <RouterLink :to="ROUTES_PATHS.private.relevantWithReplacement">Актуальные с заменой</RouterLink>
-    <RouterLink :to="ROUTES_PATHS.private.selfReplacement">Заменены сами на себя</RouterLink>
-    <RouterLink :to="ROUTES_PATHS.private.withoutBrand">Без бренда</RouterLink>
+    <RouterLink :to="ROUTES_PATHS.search">Поиск</RouterLink>
+    <RouterLink :to="ROUTES_PATHS.signIn">Войти</RouterLink>
+    <RouterLink :to="ROUTES_PATHS.duplicate">Дубликаты</RouterLink>
+    <RouterLink :to="ROUTES_PATHS.invalidReplacement">Невалидные замены</RouterLink>
+    <RouterLink :to="ROUTES_PATHS.obsoleteWithoutReplacement">Без замены</RouterLink>
+    <RouterLink :to="ROUTES_PATHS.relevantWithReplacement">Актуальные с заменой</RouterLink>
+    <RouterLink :to="ROUTES_PATHS.selfReplacement">Заменены сами на себя</RouterLink>
+    <RouterLink :to="ROUTES_PATHS.withoutBrand">Без бренда</RouterLink>
   </nav>
   <DarkModeSwitcher />
   <RouterView />
--- src/modules/core/router/router.ts
+++ src/modules/core/router/router.ts
@@ -1,7 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './routes'
+import { ROUTES_PATHS } from '@core/router/routes-paths'
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const shouldProtectRoute = to.meta.requiresAuth
+  const isSignedIn = false
+  const isSignInPage = to.href === ROUTES_PATHS.signIn
+
+  if (isSignedIn && isSignInPage) {
+    return next({ path: ROUTES_PATHS.search })
+  }
+
+  if (!isSignedIn && shouldProtectRoute) {
+    return next({ path: ROUTES_PATHS.signIn })
+  }
+
+  next()
 })
