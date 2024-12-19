a369862bc6b7a9cf9ad8ee4e9c8a09a3a6475a8e
Kirill <qtrixnet@yandex.ru>
2024-12-19 20:12:18 +0500

    feat: add border to header

--- src/shared/ui/header/index.tsx
+++ src/shared/ui/header/index.tsx
@@ -5,7 +5,7 @@ import { Navigation } from '@shared/ui/navigation'
 
 export const Header = (): ReactElement => {
   return (
-    <header className='p-6 bg-gradient-to-r from-white to-zinc-400  flex items-center justify-between gap-2'>
+    <header className='p-4 bg-gradient-to-r from-white to-zinc-300  flex items-center justify-between gap-2 mt-4 mx-4 rounded-2xl border border-border'>
       <Image src='/logo.png' width={126} height={18} alt='logo' />
 
       <Navigation />
