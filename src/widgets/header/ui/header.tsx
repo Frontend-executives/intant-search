66bdbcd175ae948390891efc446dd2f8ec0cccf4
Kirill <qtrixnet@yandex.ru>
2024-12-28 19:06:33 +0500

    feat: add header styles

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -5,10 +5,15 @@ import { Navigation } from './navigation'
 
 export const Header = (): ReactElement => {
   return (
-    <header className='p-3 bg-gradient-to-r from-white to-zinc-300  flex items-center justify-between gap-2 m-6 rounded-2xl border border-border'>
-      <Image src='/logo.png' width={126} height={18} alt='logo' />
+    <>
+      <div className='h-[66px] m-6'>1</div>
+      <div className='p-6 fixed top-0 left-0 right-0 z-10 bg-white/50 backdrop-blur-md'>
+        <header className='p-3 bg-gradient-to-r from-white/70 to-zinc-400/70  flex items-center justify-between gap-2 rounded-2xl border border-border'>
+          <Image src='/logo.png' width={126} height={18} alt='logo' />
 
-      <Navigation />
-    </header>
+          <Navigation />
+        </header>
+      </div>
+    </>
   )
 }
