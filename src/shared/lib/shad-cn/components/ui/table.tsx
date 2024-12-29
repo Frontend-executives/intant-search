b1ae084dcb4cf1345a5882b6cc89e2ffe6fd2a37
Kirill <qtrixnet@yandex.ru>
2024-12-29 22:33:51 +0500

    fix table styles

--- src/shared/lib/shad-cn/components/ui/table.tsx
+++ src/shared/lib/shad-cn/components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className='relative w-full overflow-hidden border rounded-xl flex flex-grow'>
+  <div className='relative w-full overflow-hidden border rounded-xl flex'>
     <table
       ref={ref}
       className={cn('w-full caption-bottom text-sm', className)}
